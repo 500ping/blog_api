fee6b1816efde7ea472d00a4958921d84ad96c2f
500ping <ndt512x@gmail.com>
2022-09-15 21:55:54 +0700

    fix requirements

--- app/settings.py
+++ app/settings.py
@@ -6,8 +6,8 @@ SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 if DEBUG:
     SQLALCHEMY_DATABASE_URI = "sqlite:///example.db"
 
-JWT_SECRET = "secret"
-ALGORITHM = "HS256"
+JWT_SECRET = os.getenv("JWT_SECRET", "secret")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE: int = 60 * 60 * 3  # 3 hours
 # 60 seconds * 60 minutes * 24 hours * 3 days = 3 days
 REFRESH_TOKEN_EXPIRE: int = 60 * 60 * 24 * 3
